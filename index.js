0bfae5a37b593d3c2a4803e13cec8cb7f8b78247
abner0samuel <153988389+abner0samuel@users.noreply.github.com>
2024-05-07 12:55:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const addbtn = document.querySelector("#addbtn")
-const root2 = document.querySelector(".container1")
+const addbtn = document.querySelector("#addbtn");
+const root2 = document.querySelector(".container1");
 
 function shownote() {
-    root2.innerHTML= localStorage.getItem('myNote')
+    root2.innerHTML= localStorage.getItem('myNote') 
 }
-document.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') {
-      e.preventDefault
-      console.log('enter');
+// document.addEventListener('keydown', (e) => {
+//     if (e.key == 'Enter') {
+//       e.preventDefault
+//       console.log('enter');
+//     }
+// });
+localStorage.setItem("ok","okkkkkkkkkkkkkkkkkk")
+console.log(localStorage.key(2,'ok'));
+root2.addEventListener("click",(e)=>{
+    console.log(e.target.id + "is clicked");
+    const target = e.target
+    if(target.matches("p")){
+        localStorage.clear(e.target)
+        target.style.backgroundColor ="orange"
     }
-});
+})
+root2.addEventListener("click",(e)=>{
+    const target = e.target
+    if(target.matches("button")){
+        target.style.backgroundColor ="green"
+        console.log(localStorage.removeItem(1));
+        console.log(localStorage.getItem('myNote'));
+    }
+})
 shownote()
 function app() {
     const root1 = document.createElement('div')
@@ -17,6 +35,7 @@ function app() {
     root2.appendChild(root1)
     const root = document.createElement('p')
     root.setAttribute('contenteditable', "true")
+    root.setAttribute('spellcheck', "true")
     root.setAttribute('class', 'root')
     const xbtn = document.createElement('button')
     xbtn.setAttribute('id', 'Xbtn')
@@ -41,13 +60,29 @@ function app() {
 
     savebtn.addEventListener('click', () => {
        localStorage.setItem('myNote', root2.innerHTML)
-       savebtn.innerText = 'delete'
+       savebtn.innerHTML= '&check;'
     })
    
 }
-
-
 addbtn.addEventListener('click', () => {
-    alert('are you sure you want to add note')
+    alert('add note')
     app()
 })
+
+const Storage = "item ";
+let item = 1;
+
+console.log(Storage + item);
+item++
+console.log(Storage + item);
+item++
+console.log(Storage + item);
+item++
+console.log(Storage + item);
+item++
+console.log(Storage + item);
+item++
+console.log(Storage + item);
+
+
+
